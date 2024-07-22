bc8580e92b14274efd3deb739d4c617142662bfd
alnah <alexis.nahan@gmail.com>
2024-07-22 02:10:58 -0300

    feat(jobs): add pagination and total job count to `getAllJobs` response in controller

--- src/controllers/jobs.js
+++ src/controllers/jobs.js
@@ -10,6 +10,7 @@ const getAllJobs = async (req, res, next) => {
   } = req;
 
   const query = { createdBy: userId };
+
   if (search) query.position = { $regex: search, $options: "i" };
   if (status && status !== "all") query.status = status;
   if (jobType && jobType !== "all") query.jobType = jobType;
@@ -30,8 +31,17 @@ const getAllJobs = async (req, res, next) => {
       result = result.sort("-createdAt");
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res.status(StatusCodes.OK).json({ jobs });
+  const totalJobs = await Job.countDocuments(query);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const createJob = async (req, res, next) => {
