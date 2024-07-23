6fa88d948e69c5443d82054800be32e9c0c97063
alnah <alexis.nahan@gmail.com>
2024-07-23 12:31:36 -0300

    feat(auth): add an `apiLimiter` using `express-rate-limit` to the register and login routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,19 @@
 const router = require("express").Router();
+const rateLimit = require("express-rate-limit");
 
 const { register, login, updateUser } = require("../controllers/auth");
 const authUser = require("../middlewares/auth-user");
 const testUser = require("../middlewares/test-user");
 
-router.route("/register").post(register);
-router.route("/login").post(login);
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1e3, // 15 minutes
+  limit: 10, // limit each IP to 10 requests per 15 minutes
+  standardHeaders: "draft-7", // draft-7: combined `RateLimit` header
+  legacyHeaders: false, // disable the `X-RateLimit-*` headers
+});
+
+router.route("/register").post(apiLimiter, register);
+router.route("/login").post(apiLimiter, login);
 router.route("/updateUser").patch(authUser, testUser, updateUser);
 
 module.exports = router;
